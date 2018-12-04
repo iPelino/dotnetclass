90128ea5a773f9565227c029d74d314b7d69ceac
pelino <pelmut2000@gmail.com>
2018-12-04 11:52:26 +0200

    classes

---
+++ ConsoleApp1/Cars.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cars
+    {
+        private string color;
+        private int year;
+        public int speed;
+       
+        //Constructor Overloading
+
+        public Cars()
+        {
+
+        }
+
+        public Cars(int speed,string color, int year)
+        {
+            this.speed = speed;
+            this.color = color;
+            this.year = year;
+        }
+
+        public string Make { get; set; }
+        public string Manifacturer { get; set; }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        //Method Overloading ==> Polymorphism
+        public int Accerelate(int initSpeed)
+        {
+            return speed = initSpeed + 5;
+        }
+
+        public void Accerelate()
+        {
+            
+        }
+
+       
+
+        private void Salary(double monthlySalary)
+        {
+
+        }
+
+        private void Salary(int hours, double hourlyRate)
+        {
+
+        }
+
+        private void Salary(int hours, double hourlyRate, double bonus)
+        {
+
+        }
+
+        public int Decerelate(int initSpeed)
+        {
+            return speed = initSpeed-5;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*var operation1 = new Calculate();
+            Console.WriteLine("Enter first number");
+            var num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            var num2 = Convert.ToDouble(Console.ReadLine());
+          
+         
+            Console.WriteLine("the sum of {0} and {1} is equal to {2}",
+                num1,num2, operation1.Add(num1, num2));
+
+            Console.ReadKey();*/
+
+            var car0 = new Cars();
+            car0.Color = "Yellow";
+
+            var car1 = new Cars(50,"black",2018);
+            var car2 = new Cars(70,"red",2008);
+            var car3 = new Cars(65,"Silver",2010);
+            Console.WriteLine(car3.Year);
+            Console.ReadKey();
+
+
+
+
+        }
+
+        
+    }
+
+}
